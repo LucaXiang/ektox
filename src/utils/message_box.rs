b93aa53486ff23a64f81a0f402aa736c56375dbe
LucaXiang <luca.xiang@outlook.com>
2022-04-25 00:25:06 +0200

    fix(utils/message_box): use c style string

--- src/utils/message_box.rs
+++ src/utils/message_box.rs
@@ -11,6 +11,12 @@ pub struct MessageBox;
 #[allow(unused)]
 impl MessageBox {
     fn msg(text: &str, caption: &str, style: MESSAGEBOX_STYLE) -> MESSAGEBOX_RESULT {
+        // convert to c style string
+        let mut text = text.to_owned();
+        text.push('\0');
+        let mut caption = caption.to_owned();
+        caption.push('\0');
+
         unsafe {
             MessageBoxA(
                 HWND(0),
@@ -27,5 +33,16 @@ impl MessageBox {
 
     pub fn erro(msg: &str) {
         Self::msg(msg, "Error", MB_OK);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::MessageBox;
+
+    #[test]
+    #[ignore = "message box will block test"]
+    fn it_works() {
+        MessageBox::info("Test!");
     }
 }
